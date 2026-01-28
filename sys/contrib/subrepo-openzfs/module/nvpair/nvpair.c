7fa5f5e265789acd8daa7d961acaa2f710910cf5
Brooks Davis <brooks@one-eyed-alien.net>
2026-01-28 15:25:05 +0000

    [DNM] zfs: chase xdrproc_t changes in FreeBSD/CheriBSD
    Include some workarounds while we wait for various counters to be
    bumped naturally.
    
    This will be looped back vis the subrepo

--- sys/contrib/subrepo-openzfs/module/nvpair/nvpair.c
+++ sys/contrib/subrepo-openzfs/module/nvpair/nvpair.c
@@ -3254,7 +3254,14 @@ nvs_xdr_nvl_fini(nvstream_t *nvs)
  * xdrproc_t-compatible callbacks for xdr_array()
  */
 
-#if defined(_KERNEL) && defined(__linux__) /* Linux kernel */
+/*
+ * XXX-CHERI: remove XDRPROC_TAKES_TWO_ARGS after CheriBSD version is bumped.
+ * Remove __CheriBSD_version check after __FreeBSD_version 1600010 is merged.
+ */
+#if defined(XDRPROC_TAKES_TWO_ARGS) || \
+    (defined(__CheriBSD_version) && __CheriBSD_version > 20250301) || \
+    (defined(__FreeBSD_version) && __FreeBSD_version >= 1600010) || \
+    (defined(_KERNEL) && defined(__linux__)) /* Linux kernel */
 
 #define	NVS_BUILD_XDRPROC_T(type)		\
 static bool_t					\
