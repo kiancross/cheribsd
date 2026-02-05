d3b7a9a1be68ef06cf169f0f2188f6f5bcdcf84b
Alfredo Mazzinghi <am2419@cl.cam.ac.uk>
2026-02-05 20:37:52 +0000

    cheribsdtest: Abstract away checks for existence of CHERI permission bits.

--- bin/cheribsdtest/riscv/cheribsdtest_md.h
+++ bin/cheribsdtest/riscv/cheribsdtest_md.h
@@ -55,8 +55,17 @@
 #define	XFAIL_VARARG_BOUNDS	"varargs bounds known to be unimplemented"
 #endif
 
+
 #ifdef __CHERI_TGOT_TLS__
 #define	TLS_EXACT_BOUNDS
+#endif
+
+/* Supported architecture permission bits feature flags */
+#ifdef __riscv_xcheri
+#define	HAS_CHERI_PERM_LOAD_STORE_CAP
+#else
+#define	HAS_CHERI_PERM_CAP
+#define	HAS_CHERI_PERM_LOAD_MUTABLE
 #endif
 
 #define	CAPREG_PCC(capreg)	((capreg)->sepcc)
